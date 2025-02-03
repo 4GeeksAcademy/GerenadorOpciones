1801f38ed4f383bec3d1fe48e361af8a58df7388
Alberto Delicado Delicado <45593059+exalbertod@users.noreply.github.com>
2025-02-03 18:59:38 +0000

    PushFinal

--- src/app.js
+++ src/app.js
@@ -7,5 +7,29 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let dominios = [".net", ".com", ".es"];
+
+  function combinaciones(pronoun, adjetives, names, dominios) {
+    let combis = [];
+
+    for (let pro of pronoun) {
+      for (let adj of adjetives) {
+        for (let nam of names) {
+          for (let dom of dominios) {
+            combis.push(pro + adj + nam + dom);
+          }
+        }
+      }
+    }
+    document.getElementById("listado").innerText = combis.join("\n");
+    return combis;
+  }
+  let result = combinaciones(pronoun, adj, noun, dominios);
+
+  combinaciones(pronoun, adj, noun, dominios);
+
+  console.log(result);
 };
